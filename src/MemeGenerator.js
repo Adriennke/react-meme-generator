4e0060fde8a511e82e810fcfaa1f5d3cb7ea52f5
Adrienn <adrienntollas@gmail.com>
2020-04-20 19:41:19 +0200

    add handle function to submit

--- src/MemeGenerator.js
+++ src/MemeGenerator.js
@@ -10,6 +10,9 @@ export default class MemeGenerator extends Component {
             allMemeImgs: []
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
+
+
     }
     componentDidMount() {
         fetch("https://api.imgflip.com/get_memes")
@@ -24,11 +27,17 @@ export default class MemeGenerator extends Component {
         const { name, value } = event.target
         this.setState({ [name]: value })
     }
+    handleSubmit(event) {
+        event.preventDefault()
+        const randNum = Math.floor(Math.random() * this.state.allMemeImgs.length)
+        const randMemeImg = this.state.allMemeImgs[randNum].url
+        this.setState({ randomImg: randMemeImg })
+    }
 
     render() {
         return (
             <div>
-                <form className="meme-form" action="">
+                <form className="meme-form" onSubmit={this.handleSubmit}>
                     <input
                         type="text"
                         name="topText"
@@ -43,7 +52,13 @@ export default class MemeGenerator extends Component {
                         value={this.state.bottomText}
                         onChange={this.handleChange}
                     />
+                    <button>Gen</button>
                 </form>
+                <div className="meme">
+                    <img src={this.state.randomImg} alt="" />
+                    <h2 className="top">{this.state.topText}</h2>
+                    <h2 className="bottom">{this.state.bottomText}</h2>
+                </div>
             </div>
         )
     }

6f3bd55ec86e46243358af94efa932e52af79ba2
Adrienn <adrienntollas@gmail.com>
2020-04-20 19:25:27 +0200

    Updates

---
+++ precache-manifest.2d5ea8b7478915e8cc940cfee269ebd0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "332c6017486c35ce6adabfc7468c5bdb",
+    "url": "/react-meme-generator/index.html"
+  },
+  {
+    "revision": "81667cee6f1881dc627d",
+    "url": "/react-meme-generator/static/css/main.1f0f518d.chunk.css"
+  },
+  {
+    "revision": "72f27ae787620a539d21",
+    "url": "/react-meme-generator/static/js/2.9e13ebc0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-meme-generator/static/js/2.9e13ebc0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "81667cee6f1881dc627d",
+    "url": "/react-meme-generator/static/js/main.d0f70704.chunk.js"
+  },
+  {
+    "revision": "8342e7e6e779d34a02b4",
+    "url": "/react-meme-generator/static/js/runtime-main.8d23de7a.js"
+  }
+]);
